0d655d6c102aa275266a30b10edece9f87ef2f60
wilburli99 <165343639+wilburli99@users.noreply.github.com>
2024-03-29 15:34:33 +1100

    Add files via upload

---
+++ class_1_random_Cprogram.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    printf("第一次作业：\n");
+
+    printf("********************\n");
+    printf("********begin*******\n");
+    printf("********************\n");
+
+    return 0;
+}//
+// Created by Wilbur Lee on 29/3/2024.
+//
